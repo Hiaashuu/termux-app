4744385cfc1cdad56fe6ba848c6d1fdcaee6e5ad
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-08-02 09:07:52 +0530

    Initial commit via CodXFuse

---
+++ termux-shared/src/main/java/com/termux/shared/termux/extrakeys/SpecialButtonState.java
@@ -0,0 +1,53 @@
+package com.termux.shared.termux.extrakeys;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** The {@link Class} that maintains a state of a {@link SpecialButton} */
+public class SpecialButtonState {
+
+    /** If special button has been created for the {@link ExtraKeysView}. */
+    boolean isCreated = false;
+    /** If special button is active. */
+    boolean isActive = false;
+    /** If special button is locked due to long hold on it and should not be deactivated if its
+     * state is read. */
+    boolean isLocked = false;
+
+    List<MaterialButton> buttons = new ArrayList<>();
+
+    ExtraKeysView mExtraKeysView;
+
+    /**
+     * Initialize a {@link SpecialButtonState} to maintain state of a {@link SpecialButton}.
+     *
+     * @param extraKeysView The {@link ExtraKeysView} instance in which the {@link SpecialButton}
+     *                      is to be registered.
+     */
+    public SpecialButtonState(ExtraKeysView extraKeysView) {
+        mExtraKeysView = extraKeysView;
+    }
+
+    /** Set {@link #isCreated}. */
+    public void setIsCreated(boolean value) {
+        isCreated = value;
+    }
+
+    /** Set {@link #isActive}. */
+    public void setIsActive(boolean value) {
+        isActive = value;
+        int defaultBtnBg = com.termux.shared.theme.ThemeUtils.getSystemAttrColor(mExtraKeysView.getContext(), com.termux.shared.R.attr.extraKeysButtonDefaultBg, 0x1AFFFFFF);
+        for (MaterialButton button : buttons) {
+            button.setTextColor(value ? mExtraKeysView.getButtonActiveTextColor() : mExtraKeysView.getButtonTextColor());
+            button.setBackgroundTintList(android.content.res.ColorStateList.valueOf(value ? mExtraKeysView.getButtonActiveBackgroundColor() : defaultBtnBg));
+        }
+    }
+
+    /** Set {@link #isLocked}. */
+    public void setIsLocked(boolean value) {
+        isLocked = value;
+    }
+
+}
